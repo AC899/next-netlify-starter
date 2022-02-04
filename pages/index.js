e17df8686a090cc2863fa480374ad3ce117901ca
AC89 <82317889+AC899@users.noreply.github.com>
2022-02-04 09:21:28 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,11 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+	  <img src="https://drive.google.com/uc?id=1Hi_mW_e5fZOBk47I2-C1k_M_uzWMeN_i" alt="Girl in a jacket" width="500" height="600">
+	  
+	 
       <main>
 
-function add_img() { 
-	var img = document.createElement('img'); 
-    img.src = 'https://drive.google.com/uc?id=1Hi_mW_e5fZOBk47I2-C1k_M_uzWMeN_i'; 
-	document.getElementById('body').appendChild(img);
-    
       </main>
 
       <Footer />
