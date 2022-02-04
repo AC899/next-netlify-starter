a0df3a33cf28078d430e32d00095ad80b73a28ca
AC89 <82317889+AC899@users.noreply.github.com>
2022-02-04 08:55:53 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,23 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+    
+    {
+  height: 100%;
+}
+
+.bg {
+  /* The image used */
+  background-image: url("https://drive.google.com/uc?id=1Hi_mW_e5fZOBk47I2-C1k_M_uzWMeN_i");
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
         </p>
       </main>
 
