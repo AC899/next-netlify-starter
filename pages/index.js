55bb269e4dbc5f2b833de48af6ca11f7e92ebaf2
AC89 <82317889+AC899@users.noreply.github.com>
2022-02-04 09:17:59 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,11 @@ export default function Home() {
 
       <main>
 
-<a href="default.asp">
-  <img src="https://drive.google.com/uc?id=1Hi_mW_e5fZOBk47I2-C1k_M_uzWMeN_i" alt="HTML tutorial" style="width:400px;height:400px;">
-</a>
+function add_img() { 
+	var img = document.createElement('img'); 
+    img.src = 'https://drive.google.com/uc?id=1Hi_mW_e5fZOBk47I2-C1k_M_uzWMeN_i'; 
+	document.getElementById('body').appendChild(img);
+    
       </main>
 
       <Footer />
